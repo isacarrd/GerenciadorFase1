15bfa80df1d4e84e7ba409283a2b9139299a333f
isacarrd <rcpessoaluso@gmail.com>
2026-08-21 22:12:56 -0300

    :construction: em desenvolvimento

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
-import "./App.css";
+import "./index.css";
+import Header from "./components/ui/Header";
+import SearchBar from "./components/form/SearchBar";
 
 export default function App() {
   return (
-    <>
-      <h1>Olá</h1>
-      <p>Oi?</p>
-    </>
+    <div className="bg-(--bgColor)">
+      <Header />
+      <main className="flex flex-col px-6 py-12 gap-12 lg:px-25 lg:py-14 lg:gap-30">
+        <SearchBar/>
+      </main>
+    </div>
   );
 }
-
-
