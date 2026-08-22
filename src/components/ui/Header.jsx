8fccb6ac82c999f6b4af01edacb3cbd8dd8255ef
isacarrd <rcpessoaluso@gmail.com>
2026-08-21 22:07:51 -0300

    update: componente header

---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,10 @@
+import icon from '../../assets/icon.svg'
+
+export default function Header() {
+  return (
+    <header className="px-6 py-5 gap-1.5 bg-(--verdePrim) flex flex-row items-center lg:gap-3 lg:px-25 lg:py-10 ">
+      <img src={icon} alt="Icon" className="w-6 lg:w-16" />
+      <p className="font-sans text-xs lg:text-2xl text-(--preto)">Admin</p>
+    </header>
+  );
+}
