c8c79a3e7ade64aa4ef301ffdc818319981bcea4
isacarrd <rcpessoaluso@gmail.com>
2026-08-24 22:50:33 -0300

    :test_tube: test: testando os componentes visualmente

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./index.css";
 import Header from "./components/ui/Header";
-import SearchBar from "./components/form/SearchBar";
+import SearchSection from "./components/form/SearchSection";
+import ProductSection from "./components/form/ProductSection";
 
 export default function App() {
   return (
     <div className="bg-(--bgColor)">
       <Header />
       <main className="flex flex-col px-6 py-12 gap-12 lg:px-25 lg:py-14 lg:gap-30">
-        <SearchBar/>
+        <SearchSection />
+        <ProductSection />
       </main>
     </div>
   );
