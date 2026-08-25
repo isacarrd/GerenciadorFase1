2c4d04c3c4bfc80b78fc8e6298ce3f264ec34a0e
isacarrd <rcpessoaluso@gmail.com>
2026-08-24 22:45:19 -0300

    :lipstick: feat: add fonte Inter

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -4,7 +4,9 @@ export default function Header() {
   return (
     <header className="px-6 py-5 gap-1.5 bg-(--verdePrim) flex flex-row items-center lg:gap-3 lg:px-25 lg:py-10 ">
       <img src={icon} alt="Icon" className="w-6 lg:w-16" />
-      <p className="font-sans text-xs lg:text-2xl text-(--preto)">Admin</p>
+      <p className="text-xs lg:text-2xl text-(--preto) font-inter">
+        Admin
+      </p>
     </header>
   );
 }
