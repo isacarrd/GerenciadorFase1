d7c390cce182a8c4bfaee42a92fe772bd8c80ee7
isacarrd <rcpessoaluso@gmail.com>
2026-08-25 23:11:16 -0300

    :construction: componente de Categoria do Produto com condicional

--- src/components/ui/CategorySpan.jsx
+++ src/components/ui/CategorySpan.jsx
@@ -5,3 +5,19 @@ export function CategorySpanMain({ children }) {
     </span>
   );
 }
+
+export function CategoryProduct({ children, active }) {
+  if (active) {
+    return (<span className="font-inter font-bold text-[10px] lg:text-sm text-(--preto) bg-(--cinza) rounded-full border-2 border-(--cinza) px-1 py-1 lg:px-2 lg:py-2">
+      {children}
+    </span>)
+  } else {
+    return (
+      <span className="font-inter font-bold text-[10px] lg:text-sm text-(--noEstoque) bg-(--cinza) rounded-full border-2 border-(--cinza) cursor-not-allowed px-1 py-1 lg:px-2 lg:py-2">
+        {children}
+      </span>
+    );
+  }
+}
+
+export function SelectCategory({ children }) {}
