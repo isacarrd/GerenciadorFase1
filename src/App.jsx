95d70f406ad3d5b5282a1718a59f02457691bc7e
isacarrd <rcpessoaluso@gmail.com>
2026-08-26 22:43:46 -0300

    :test_tube: test: testando os componentes visualmente

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import "./index.css";
 import Header from "./components/ui/Header";
 import SearchSection from "./components/form/SearchSection";
 import ProductSection from "./components/form/ProductSection";
+import CreateProduct from "./components/form/CreateProduct";
 
 export default function App() {
   return (
     <div className="bg-(--bgColor)">
       <Header />
       <main className="flex flex-col px-6 py-12 gap-12 lg:px-25 lg:py-14 lg:gap-30">
+        <CreateProduct isOpen={true} />
         <SearchSection />
-        <ProductSection />
+        <ProductSection /> 
       </main>
     </div>
   );
