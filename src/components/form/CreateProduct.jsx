b5011a57ad8a2324dde28f9642dae3cec32f4446
isacarrd <rcpessoaluso@gmail.com>
2026-08-27 23:04:53 -0300

    :rocket:	add campos Descrição, Categoria e Estoque

--- src/components/form/CreateProduct.jsx
+++ src/components/form/CreateProduct.jsx
@@ -1,8 +1,21 @@
-import uploadIcon from '../../assets/upload.svg'
-import required from '../../assets/required.svg'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import required from "../../assets/required.svg";
+import uploadIcon from "../../assets/upload.svg";
+import { useCharCounter } from "../../data/useCharCounter";
+
+import CategoryCamp from "./CategoryCamp";
 
 export default function CreateProduct({ isOpen, onClose }) {
+  const [prodNome, setProdNome] = useState("");
+  const [prodDesc, setProdDesc] = useState("");
+  const [prodCateg, setProdCateg] = useState([]); // Será retornado um array, exemplo ['CPU', 'Computadores', 'Hardware'] onde uso o select
+  const [prodQuant, setProdQuant] = useState("");
+
+  // Contadores para cada campo específico
+  const nomeCounter = useCharCounter(prodNome, setProdNome, 50);
+  const descCounter = useCharCounter(prodDesc, setProdDesc, 200);
+  const categCounter = useCharCounter(prodCateg, setProdCateg, 5);
+
   if (!isOpen) return null;
 
   // função de acessibilidade
@@ -24,13 +37,13 @@ export default function CreateProduct({ isOpen, onClose }) {
     };
   }, [isOpen, onClose]);
 
-  if (!isOpen) return null;
   return (
     <div className="min-w-68.75 max-w-270 bg-(--branco) h-fit flex flex-col items-star px-6 py-6 gap-6 lg:gap-8 rounded-[10px] lg:rounded-2xl">
       <h2 className="break-all font-inter font-bold text-(--preto) text-2xl lg:text-[32px]">
         NOVO PRODUTO
       </h2>
       <div className="elements w-full flex flex-col gap-2.5 lg:gap-5">
+        {/* Imagem */}
         <div className="imagem flex flex-col gap-2">
           <label
             htmlFor="imgProduto"
@@ -55,6 +68,8 @@ export default function CreateProduct({ isOpen, onClose }) {
             className="hidden"
           />
         </div>
+
+        {/* Nome */}
         <div className="nome w-full flex flex-col gap-1 lg:gap-2">
           <span className="flex flex-row items-center justify-between flex-wrap">
             <label
@@ -69,18 +84,82 @@ export default function CreateProduct({ isOpen, onClose }) {
               />
             </label>
             <span className="font-inter font-medium text-(--noEstoque) text-sm lg:text-xl">
-              0/50
+              {nomeCounter.remaining}/{nomeCounter.limit}
             </span>
           </span>
           <input
-            required
             type="text"
-            name="nomeProduto"
             id="nomeProduto"
+            name="nomeProduto"
+            value={prodNome}
+            required
+            maxLength={nomeCounter.limit}
+            onChange={nomeCounter.handleChange}
             placeholder="Nome do Produto"
             className="font-inter text-sm focus:outline-none w-full px-2 py-2 bg-(--cinza) text-(--preto) rounded-lg placeholder:font-inter
             placeholder:text-(--preto) placeholder:text-sm lg:placeholder:text-xl lg:rounded-2xl lg:text-xl lg:px-4 lg:py-4"
           />
+        </div>
+
+        {/* Descrição */}
+        <div className="descricao w-full flex flex-col gap-1 lg:gap-2">
+          <span className="flex flex-row items-center justify-between flex-wrap">
+            <label
+              htmlFor="descProduto"
+              className="font-inter font-bold text-sm lg:text-xl flex justify-start"
+            >
+              Descrição
+              <img
+                src={required}
+                alt="Required Warning"
+                className="w-3 h-3 lg:w-4 lg:h-4"
+              />
+            </label>
+            <span className="font-inter font-medium text-(--noEstoque) text-sm lg:text-xl">
+              {descCounter.remaining}/{descCounter.limit}
+            </span>
+          </span>
+          <textarea
+            type="text"
+            id="descProduto"
+            name="descProduto"
+            value={prodDesc}
+            required
+            maxLength={descCounter.limit}
+            onChange={descCounter.handleChange}
+            placeholder="Descrição do Produto"
+            className="font-inter text-sm focus:outline-none w-full px-2 py-2 bg-(--cinza) text-(--preto) rounded-lg placeholder:font-inter
+            placeholder:text-(--preto) placeholder:text-sm  scrollbar-thumb-red-50 lg:placeholder:text-xl lg:rounded-2xl lg:text-xl lg:px-4 lg:py-4"
+          />
+        </div>
+
+        <CategoryCamp categorias={prodCateg} counter={categCounter} />
+
+        {/* Estoque/Quantidade */}
+        <div className="quantidade w-full flex flex-row items-center gap-1 lg:gap-2">
+          <label
+            htmlFor="quantProduto"
+            className="font-inter font-bold text-sm lg:text-xl flex justify-start"
+          >
+            Estoque:
+          </label>
+          <div className="w-fit flex flex-row gap-0.5">
+            <input
+              type="number"
+              id="quantProduto"
+              name="quantProduto"
+              value={prodQuant}
+              required
+              onChange={(e) => setProdQuant(e.target.value)}
+              placeholder="00"
+              className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none w-min-[50px] lg:w-min-[57px] font-inter text-sm lg:text-xl px-2 py-2 lg:px-4 lg:py-4 bg-(--cinza) text-(--preto) rounded-lg lg:rounded-2xl placeholder:font-inter placeholder:text-(--preto) placeholder:text-sm lg:placeholder:text-xl focus:outline-none"
+            />
+            <img
+              src={required}
+              alt="Required Warning"
+              className="w-3 h-3 lg:w-4 lg:h-4"
+            />
+          </div>
         </div>
       </div>
     </div>
