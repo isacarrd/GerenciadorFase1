58fd6c63b4eb95d354703b298767363db11d5f37
isacarrd <rcpessoaluso@gmail.com>
2026-08-28 22:52:21 -0300

    :bricks: ci: base de dados fictícia

---
+++ src/data/listaProdutosTeste.js
@@ -0,0 +1,38 @@
+export const produtosTeste = [
+  {
+    id: 1,
+    nomeProduto: "RX9060 XT 16gb",
+    descProduto: "Placa de vídeo AMD nível Intermediário",
+    imgProduto:
+      "https://images5.kabum.com.br/produtos/fotos/779095/placa-de-video-asrock-rx9060xt-cl-16go_1748868930_gg.jpg",
+    categProduto: ["Hardware", "GPU", "AMD"],
+    quantProduto: 10,
+  },
+  {
+    id: 2,
+    nomeProduto: "RTX5060 8gb",
+    descProduto: "Placa de vídeo NVIDIA nível Básico",
+    imgProduto:
+      "https://images1.kabum.com.br/produtos/fotos/1000741/placa-de-video-msi-rtx-5060-shadow-2x-oc-nvidia-geforce-8gb-gddr7-128-bit-912-v537-038_1771333499_gg.jpg",
+    categProduto: ["Hardware", "GPU", "NVIDIA"],
+    quantProduto: 0,
+  },
+  {
+    id: 3,
+    nomeProduto: "Blusa do Brasil",
+    descProduto: "FIFA 2026",
+    imgProduto:
+      "https://stories.cnnbrasil.com.br/wp-content/uploads/sites/9/2026/03/nova-camisa-oficial-selecao-2026-vinijr.jpg",
+    categProduto: ["Roupa", "Esporte"],
+    quantProduto: 0,
+  },
+  {
+    id: 4,
+    nomeProduto: "Copo de plástico",
+    descProduto: "Copo preto de plástico fosco.",
+    imgProduto:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_748888-MLB89576682471_082025-F-kit-50-copos-ecologico-lisos-pp-400ml-varias-cores-festas.webp",
+    categProduto: ["Casa"],
+    quantProduto: 0,
+  },
+];
