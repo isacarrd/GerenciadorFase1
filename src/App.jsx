3bab5cc6fc3425dcccf8c356cbcf0c776acba32d
isacarrd <rcpessoaluso@gmail.com>
2026-08-31 23:38:24 -0300

    :broom: cleanup: tirando os componentes de category

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Header from "./components/ui/Header";
 import SearchSection from "./components/form/SearchSection";
 import ProductSection from "./components/form/ProductSection";
 import CreateProduct from "./components/form/CreateProduct";
-import { SelectCategory } from "./components/ui/CategorySpan";
+import {
+  SelectCategory,
+  SelectCategoryCreate,
+} from "./components/ui/CategorySpan";
 
 export default function App() {
   return (
     <div className="bg-(--bgColor)">
       <Header />
       <main className="flex flex-col px-6 py-12 gap-12 lg:px-25 lg:py-14 lg:gap-30">
-        <SelectCategory />
         <CreateProduct isOpen={true} />
         <SearchSection />
         <ProductSection /> 
