4e1b00c7df4e902b352862f142484530ee83924c
isacarrd <rcpessoaluso@gmail.com>
2026-09-03 20:50:19 -0300

    :sparkles: feat: adição do uso da função de validação + limpando campos após envio

--- src/components/form/EditProduct.jsx
+++ src/components/form/EditProduct.jsx
@@ -2,37 +2,48 @@ import { useEffect, useState } from "react";
 import required from "../../assets/required.svg";
 import uploadIcon from "../../assets/upload.svg";
 import { useCharCounter } from "../../data/useCharCounter";
+import { validarCampos } from "../../data/validacaoSimples";
 
 import CategoryCamp from "./CategoryCamp";
 
-export default function EditProduct({ id,isOpen, onClose }) {
-  const [prodNome, setProdNome] = useState("");
-  const [prodDesc, setProdDesc] = useState("");
-  const [prodCateg, setProdCateg] = useState([]); // Será retornado um array, exemplo ['CPU', 'Computadores', 'Hardware'] onde uso o select
-  const [prodQuant, setProdQuant] = useState("");
+export default function EditProduct({ id, isOpen, onClose }) {
+  const [prodImgNova, setProdImgNova] = useState("")
+  const [prodNomeNova, setProdNomeNova] = useState("");
+  const [prodDescNova, setProdDescNova] = useState("");
+  const [prodCategNova, setProdCategNova] = useState([]); // Será retornado um array, exemplo ['CPU', 'Computadores', 'Hardware'] onde uso o select
+  const [prodQuantNova, setProdQuantNova] = useState("");
 
   // Contadores para cada campo específico
-  const nomeCounter = useCharCounter(prodNome, setProdNome, 50);
-  const descCounter = useCharCounter(prodDesc, setProdDesc, 200);
-  const categCounter = useCharCounter(prodCateg, setProdCateg, 5);
+  const nomeCounter = useCharCounter(prodNomeNova, setProdNomeNova, 50);
+  const descCounter = useCharCounter(prodDescNova, setProdDescNova, 200);
+  const categCounter = useCharCounter(prodCategNova, setProdCategNova, 5);
 
-  if (!isOpen) return null;
+  const handleValidarCampos = () => {
+    validarCampos(prodNomeNova, prodDescNova, prodCategNova, prodQuantNova);
+    setProdImgNova("")
+    setProdNomeNova("")
+    setProdDescNova("")
+    setProdCategNova([])
+    setProdQuantNova("")
+  }
 
   // função de acessibilidade
   useEffect(() => {
     const handleKeyDown = (e) => {
       if (e.key === "Escape") onClose();
     };
-
+    
     if (isOpen) {
       document.addEventListener("keydown", handleKeyDown);
     }
-
+    
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
   }, [isOpen, onClose]);
 
+  if (!isOpen) return null;
+  
   return (
     <div className="w-screen h-svh fixed top-0 left-0 z-9999 flex items-center justify-center bg-black/50">
       <div className="scrollbar-thumb-(--verdeSec) scrollbar-track-transparent min-w-68.75 max-w-270 bg-(--branco) h-fit max-h-[95vh] overflow-y-auto flex flex-col items-start px-6 py-6 gap-6 lg:gap-8 rounded-[10px] lg:rounded-2xl">
@@ -61,6 +72,8 @@ export default function EditProduct({ id,isOpen, onClose }) {
             <input
               id="imgProduto"
               type="file"
+              value={prodImgNova}
+              onChange={setProdImgNova}
               accept="image/*"
               className="hidden"
             />
@@ -87,7 +100,7 @@ export default function EditProduct({ id,isOpen, onClose }) {
               type="text"
               id="nomeProduto"
               name="nomeProduto"
-              value={prodNome}
+              value={prodNomeNova}
               required
               maxLength={nomeCounter.limit}
               onChange={nomeCounter.handleChange}
@@ -118,7 +131,7 @@ export default function EditProduct({ id,isOpen, onClose }) {
               type="text"
               id="descProduto"
               name="descProduto"
-              value={prodDesc}
+              value={prodDescNova}
               required
               maxLength={descCounter.limit}
               onChange={descCounter.handleChange}
@@ -127,7 +140,7 @@ export default function EditProduct({ id,isOpen, onClose }) {
               placeholder:text-(--preto) placeholder:text-sm  scrollbar-thumb-red-50 lg:placeholder:text-xl lg:rounded-2xl lg:text-xl lg:px-4 lg:py-4"
             />
           </div>
-          <CategoryCamp categorias={prodCateg} counter={categCounter} />
+          <CategoryCamp categorias={prodCategNova} counter={categCounter} />
           {/* Estoque/Quantidade */}
           <div className="quantidade w-full flex flex-row items-center gap-1 lg:gap-2">
             <label
@@ -141,9 +154,9 @@ export default function EditProduct({ id,isOpen, onClose }) {
                 type="number"
                 id="quantProduto"
                 name="quantProduto"
-                value={prodQuant}
+                value={prodQuantNova}
                 required
-                onChange={(e) => setProdQuant(e.target.value)}
+                onChange={(e) => setProdQuantNova(e.target.value)}
                 placeholder="00"
                 className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none w-min-[50px] lg:w-min-[57px] font-inter text-sm lg:text-xl px-2 py-2 lg:px-4 lg:py-4 bg-(--cinza) text-(--preto) rounded-lg lg:rounded-2xl placeholder:font-inter placeholder:text-(--preto) placeholder:text-sm lg:placeholder:text-xl focus:outline-none"
               />
@@ -173,7 +186,7 @@ export default function EditProduct({ id,isOpen, onClose }) {
             type="button"
             id="btnCreate"
             className="cursor-pointer border-2 border-(--verdePrim) rounded-[5px] p-3 text-(--branco) bg-(--verdePrim) hover:text-(--preto)"
-            onClick={(e) => console.log("teste")}
+            onClick={handleValidarCampos}
             aria-label="Botão de criar Produto"
           >
             Criar
