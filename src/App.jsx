70c1676bee80d5f848db2f715e84f2b67097e299
isacarrd <rcpessoaluso@gmail.com>
2026-09-02 21:03:44 -0300

    :lipstick: feat: altura do App

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import EditProduct from "./components/form/EditProduct";
 
 export default function App() {
   return (
-    <div className="bg-(--bgColor)">
+    <div className="h-lvh bg-(--bgColor)">
       <Header />
       <main className="flex flex-col px-6 py-12 gap-12 lg:px-25 lg:py-14 lg:gap-30">
         <SearchSection />
