39bdf2f5b464dd59453540d6b7960db6604b5d28
isacarrd <rcpessoaluso@gmail.com>
2026-09-08 20:32:34 -0300

    :test_tube: test: testando a altura do fundo do modal

--- src/components/form/CreateProduct.jsx
+++ src/components/form/CreateProduct.jsx
@@ -48,8 +48,8 @@ export default function CreateProduct({ isOpen, onClose }) {
   if (!isOpen) return null; 
 
   return (
-    <div className="w-screen h-svh fixed top-0 left-0 z-9999 flex items-center justify-center bg-black/50">
-      <div className=" scrollbar-thumb-(--verdeSec) scrollbar-track-transparent min-w-68.75 max-w-270 bg-(--branco) h-fit max-h-[95vh] overflow-y-auto flex flex-col items-start px-6 py-6 gap-6 lg:gap-8 rounded-[10px] lg:rounded-2xl">
+    <div className="w-screen h-dvh fixed top-0 left-0 z-9999 flex items-center justify-center bg-black/50">
+      <div className="scrollbar-thumb-(--verdeSec) scrollbar-track-transparent min-w-68.75 max-w-270 bg-(--branco) h-fit max-h-[95vh] overflow-y-auto flex flex-col items-start px-6 py-6 gap-6 lg:gap-8 rounded-[10px] lg:rounded-2xl">
         <h2 className="break-all font-inter font-bold text-(--preto) text-2xl lg:text-[32px]">
           NOVO PRODUTO
         </h2>
