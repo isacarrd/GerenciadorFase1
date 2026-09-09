abe862592bd25b07d773e2b6c684b31379529f2d
isacarrd <rcpessoaluso@gmail.com>
2026-09-09 20:10:14 -0300

    :boom: fix: correção do problema de largura da div principal + estoque

--- src/components/form/CreateProduct.jsx
+++ src/components/form/CreateProduct.jsx
@@ -49,10 +49,12 @@ export default function CreateProduct({ isOpen, onClose }) {
 
   return (
     <div className="w-screen h-dvh fixed top-0 left-0 z-9999 flex items-center justify-center bg-black/50">
-      <div className="scrollbar-thumb-(--verdeSec) scrollbar-track-transparent 
-      w-68.75 lg:w-2xl 
+      <div
+        className="scrollbar-thumb-(--verdeSec) scrollbar-track-transparent 
+      w-75 lg:w-2xl 
       bg-(--branco) h-fit 
-      max-h-[95vh] overflow-y-auto flex flex-col items-start px-6 py-6 gap-6 lg:gap-8 rounded-[10px] lg:rounded-2xl">
+      max-h-[95vh] overflow-y-auto flex flex-col items-start px-6 py-6 gap-6 lg:gap-8 rounded-[10px] lg:rounded-2xl"
+      >
         <h2 className="break-all font-inter font-bold text-(--preto) text-2xl lg:text-[32px]">
           NOVO PRODUTO
         </h2>
@@ -155,7 +157,7 @@ export default function CreateProduct({ isOpen, onClose }) {
             >
               Estoque:
             </label>
-            <div className="w-fit flex flex-row gap-0.5">
+            <div className="flex flex-row gap-0.5">
               <input
                 type="number"
                 id="quantProduto"
@@ -164,7 +166,7 @@ export default function CreateProduct({ isOpen, onClose }) {
                 required
                 onChange={(e) => setProdQuant(e.target.value)}
                 placeholder="00"
-                className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none w-min-[50px] lg:w-min-[57px] font-inter text-sm lg:text-xl px-2 py-2 lg:px-4 lg:py-4 bg-(--cinza) text-(--preto) rounded-lg lg:rounded-2xl placeholder:font-inter placeholder:text-(--preto) placeholder:text-sm lg:placeholder:text-xl focus:outline-none"
+                className="w-14 lg:w-20 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none font-inter text-sm lg:text-xl px-2 py-2 lg:px-4 lg:py-4 bg-(--cinza) text-(--preto) rounded-lg lg:rounded-2xl placeholder:font-inter placeholder:text-(--preto) placeholder:text-sm lg:placeholder:text-xl focus:outline-none"
               />
               <img
                 src={required}
