0199049fbd88427fd53ad4c05027f843bef93d80
isacarrd <rcpessoaluso@gmail.com>
2026-09-09 22:15:16 -0300

    :test_tube: test: testando o estilo do CreateCategory

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./index.css";
 import Header from "./components/ui/Header";
 import ProductSection from "./components/screen/ProductSection";
 import SearchSection from "./components/screen/SearchSection";
+import CreateCategory from "./components/form/CreateCategory";
 
 export default function App() {
   return (
     <div className="h-lvh bg-(--bgColor)">
       <Header />
       <main className="flex flex-col px-6 py-12 gap-12 lg:px-25 lg:py-14 lg:gap-30">
+        <CreateCategory isOpen={true}/>
         <SearchSection />
         <ProductSection />
       </main>
