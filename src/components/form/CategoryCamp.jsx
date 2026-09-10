682ac497e7ebbc9bd1718a581fcd3b308d059bbe
isacarrd <rcpessoaluso@gmail.com>
2026-09-10 20:53:21 -0300

    :sparkles: feat: utilizando componente CreateCategory

--- src/components/form/CategoryCamp.jsx
+++ src/components/form/CategoryCamp.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import addCategory from "../../assets/create.svg";
 import required from "../../assets/required.svg";
 import { SelectCategoryCreate } from "../ui/CategorySpan";
+import CreateCategory from "./CreateCategory";
 
 export default function CategoryCamp({ categorias, counter }) {
+  const [modalCriar, setModalCriarAtivo] = useState(null);
+  const handleClose = () => {
+    setModalCriarAtivo(null);
+  };
+
   // Adiciona um "slot" vazio para um novo Select
   const adicionarCampo = () => {
     if (counter.remaining > 0) {
@@ -48,17 +55,25 @@ export default function CategoryCamp({ categorias, counter }) {
 
       <div className="w-full h-fit flex-wrap flex flex-row items-center px-2 py-2 gap-2 lg:px-4 lg:py-4 bg-(--cinza) rounded-lg lg:rounded-2xl">
         {/* Botão de Adicionar (+) */}
-        <button
-          onClick={adicionarCampo}
-          disabled={counter.remaining === 0}
-          className="cursor-pointer bg-(--verdePrim) w-8 h-8 lg:w-14 lg:h-14 rounded-[10px] lg:rounded-[15px] flex items-center justify-center disabled:bg-(--noEstoque)"
-        >
-          <img
-            src={addCategory}
-            alt="Adicionar Categoria"
-            className="w-3 h-3 lg:w-6 lg:h-6"
-          />
-        </button>
+        <div className="flex flex-col gap-2">
+          <button
+            onClick={adicionarCampo}
+            disabled={counter.remaining === 0}
+            className="cursor-pointer bg-(--verdePrim) w-8 h-8 lg:w-14 lg:h-14 rounded-[10px] lg:rounded-[15px] flex items-center justify-center disabled:bg-(--noEstoque)"
+          >
+            <img
+              src={addCategory}
+              alt="Adicionar Categoria"
+              className="w-3 h-3 lg:w-6 lg:h-6"
+            />
+          </button>
+          <button
+            onClick={() => setModalCriarAtivo(true)}
+            className="cursor-pointer font-inter font-bold text-[10px] lg:text-sm text-(--verdeSec) bg-(--branco) rounded-full border border-(--verdeSec) px-1 py-1 lg:px-2 lg:py-2 hover:text-(--preto) hover:border-(--preto)"
+          >
+            Criar Categoria
+          </button>
+        </div>
 
         {/* Tags de Categorias Adicionadas */}
 
@@ -78,6 +93,7 @@ export default function CategoryCamp({ categorias, counter }) {
             />
           ))
         )}
+        {modalCriar && <CreateCategory isOpen={true} onClose={handleClose} />}
       </div>
     </div>
   );
