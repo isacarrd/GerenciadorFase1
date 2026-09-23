6f3d8835be9e8bc7d8e5d2fe57d8446fef29d518
isacarrd <rcpessoaluso@gmail.com>
2026-09-23 20:55:24 -0300

    :sparkles: feat: evitando a criação de categorias vazias

--- src/data/validacaoSimples.js
+++ src/data/validacaoSimples.js
@@ -1,8 +1,11 @@
 export function validarCampos(nomeProd, descProd, categProd, quantProd) {
+  const categVazia = categProd.includes('')
+  
   let alertMessage = ""
   if (nomeProd == "") alertMessage += 'Nome do produto não pode ficar vazio. \n'
   if (descProd == "") alertMessage += 'A descrição do produto não pode ficar vazia. \n'
   if (categProd.length == 0) alertMessage += 'O produto precisa ter pelo menos uma categoria. \n'
+  if (categVazia) alertMessage += 'Selecione uma categoria válida. \n'
   if (quantProd < 0) alertMessage += 'A quantidade do produto não pode ser MENOR do que zero. \n'
 
   if (alertMessage != '') {
