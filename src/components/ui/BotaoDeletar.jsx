25ba9454e33dcd4cd4d6d9d31e7c3054041c23b8
isacarrd <rcpessoaluso@gmail.com>
2026-09-22 23:07:01 -0300

    :sparkles: feat: lógica de exclusão passada por props [BtnDelete]

--- src/components/ui/BotaoDeletar.jsx
+++ src/components/ui/BotaoDeletar.jsx
@@ -2,9 +2,9 @@ import deleteIco from "../../assets/delete.svg";
 import removeIco from "../../assets/remove.svg";
 
 // Btn dos Cards
-export function BtnDelete() {
+export function BtnDelete({ onDelete }) {
   return (
-    <button className="cursor-pointer w-fit h-fit flex p-1 border lg:p-2 lg:border-2 border-(--error) rounded-[20px] lg:rounded-[40px] hover:bg-(--cinza)">
+    <button onClick={onDelete} className="cursor-pointer w-fit h-fit flex p-1 border lg:p-2 lg:border-2 border-(--error) rounded-[20px] lg:rounded-[40px] hover:bg-(--cinza)">
       <img src={deleteIco} alt="Deletar" className="w-4 h-4 lg:w-8 lg:h-8" />
     </button>
   );
