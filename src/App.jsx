94ea25227c3678c1c80048faf0048fd007a3a994
isacarrd <rcpessoaluso@gmail.com>
2026-09-22 23:02:56 -0300

    :bricks: ci: passando a estrutura de dados através do pai

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import "./index.css";
+import { useState } from "react";
 import Header from "./components/ui/Header";
 import ProductSection from "./components/screen/ProductSection";
 import SearchSection from "./components/screen/SearchSection";
-import CreateCategory from "./components/form/CreateCategory";
-
-// Teste
+import { produtosTeste } from "./data/listaProdutosTeste";
 
 export default function App() {
+  const [produtos, setProdutos] = useState(produtosTeste)
+  
   return (
     <div className="h-lvh bg-(--bgColor)">
       <Header />
       <main className="flex flex-col px-6 py-12 gap-12 lg:px-25 lg:py-14 lg:gap-30">
-        {/* <CreateCategory isOpen={true}/> */}
-        <SearchSection />
-        <ProductSection />
+        <SearchSection produtos={produtos} />
+        <ProductSection produtos={produtos} setProdutos={setProdutos} />
       </main>
     </div>
   );
